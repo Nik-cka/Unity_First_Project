7f6f5475d285fb8727387017c264d3612ff99843
Nik-cka <144733681+Nik-cka@users.noreply.github.com>
2023-11-16 20:19:21 +0400

    Add files via upload

---
+++ Crickets.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello Man");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Выводит каждый кадр слово "сверчок".
+        print("crickets");
+    }
+}
---
+++ DestroyObj.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Code
+{
+    public class DestroyObj : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {   //Создает Стива из Кубов в Unity.
+            GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0,0,0);
+            GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f,1,0);
+            GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0.5f,1,0);
+            GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0.5f,2,0);
+            GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f,2,0);
+            GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0,3,0);
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
