1fb7bd4c3f0e0af2847c38d1783566d43d9e52fd
Nik-cka <144733681+Nik-cka@users.noreply.github.com>
2023-11-16 20:20:36 +0400

    Steve.cs

--- Steve.cs
+++ Steve.cs
@@ -7,6 +7,12 @@ public class steve : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+    GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0,0,0);
+    GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f,1,0);
+    GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0.5f,1,0);
+    GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0.5f,2,0);
+    GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(-0.5f,2,0);
+    GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = new Vector3(0,3,0);
         
     }
 
